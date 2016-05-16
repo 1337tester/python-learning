13dc15e7e611e3f0dd74040ae70c903285c26c4d
1337tester <328uI2k9xK>
2016-05-16 21:53:40 +0200

    updated web-selenium.py

--- test.py
+++ test.py
@@ -1,10 +1,20 @@
-count = 0
+#!/usr/bin/python
+
+import os, glob, Image
+
+pic_list = []
+
+pic_list = glob.glob('*.JPG')
+pic_list.sort()
+
+print (len(pic_list), 'images found...')
+for temp in pic_list:
+    print (temp)
 
 
-def show_count():
-    print("count = ", count)
+cmd = 'fulla -g 0.0216776:-0.0799067:0.0566601:1 '
+for temp in pic_list:
+    repair = cmd + temp
+    os.popen(repair).read()
 
 
-def set_count(t):
-    global count
-    count = t
