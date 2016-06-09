74fff83e9ce22965bd22bddac4cace2db4afd0f5
1337tester <328uI2k9xK>
2016-06-09 17:34:53 +0200

    let loose of xml parsing, adjusting it as a text file

--- creating_xml.py
+++ creating_xml.py
@@ -1,26 +1,32 @@
 __author__ = 'e1063127'
-import xml.etree.ElementTree as etree
 import time
 
-tree = etree.parse('c:\Testing/test_files/aaa.xml')
-root = tree.getroot()
-print (root)
-print (tree.findall('{http://www.w3.org/2005/Atom}end'))
-cifra = tree.find('.//{http://www.w3.org/2005/Atom}cifra')
+def replace_all(text, dic):
+    for i, j in dic.items():
+        text = text.replace(i, j)
+    return text
 
 cas = str(time.time())
-print (cas[4:10])
-cifra.text = 'ZIM' + cas[4:10]
+# print (cas)
+newID = 'ZIM' + cas[0:10] + 'aaa'
 
 
-tree.write('c:\Testing/test_files/bbb.xml')
+file1 = 'inbound_pacs_008_PF_MS_CSAG_whitelist.xml'
+file2 = 'inbound_pacs_008_PF_MS_CSAG_whitelist_' + newID + '.xml'
+
+orig_file = open(file1, 'r')
+
+orig_text = orig_file.read()
+zoznam = {'{MsgId}':newID,
+          '{InstrId}':newID,
+          '{TxId}':newID}
+new_text = replace_all(orig_text, zoznam)
 
 
-"""fi = open('c:\Testing/test_files/aaa.xml', 'r')
-fo = open('c:\Testing/test_files/bbb.xml', "w")
-lines = fi.readlines()
-fo.writelines(lines)
+new_file = open(file2, 'w')
+new_file.write(new_text)
 
-fi.close()
-fo.close()"""
+orig_file.close()
+new_file.close()
+
 
