442377a4235ce8fa8e945e7dfc0f0f0e699cd836
Michal <Zima>
2017-06-24 19:43:22 +0200

    Migrated my repo into github after 2 years break

--- test.py
+++ test.py
@@ -1,20 +1,25 @@
-#!/usr/bin/python
+import Tkinter as tk
 
-import os, glob, Image
+class ExampleApp(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self.label = tk.Label(self, text="", width=10)
+        self.label.pack()
+        self.remaining = 0
+        self.countdown(10)
 
-pic_list = []
+    def countdown(self, remaining = None):
+        if remaining is not None:
+            self.remaining = remaining
 
-pic_list = glob.glob('*.JPG')
-pic_list.sort()
+        if self.remaining <= 0:
+            self.label.configure(text="time's up!")
+        else:
+            self.label.configure(text="%d" % self.remaining)
+            self.remaining = self.remaining - 1
+            self.after(1000, self.countdown)
 
-print (len(pic_list), 'images found...')
-for temp in pic_list:
-    print (temp)
-
-
-cmd = 'fulla -g 0.0216776:-0.0799067:0.0566601:1 '
-for temp in pic_list:
-    repair = cmd + temp
-    os.popen(repair).read()
-
+if __name__ == "__main__":
+    app = ExampleApp()
+    app.mainloop()
 
