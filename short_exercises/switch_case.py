0f0720f3c825ef3fb5894e616a7946816b5e4a04
1337tester <miso.zima@gmail.com>
2018-09-13 18:51:55 +0100

    Add instaboy.py

---
+++ short_exercises/switch_case.py
@@ -0,0 +1,11 @@
+def dispatch_dict(operator, x, y):
+    return {
+        'add': lambda: x + y,
+        'subt': lambda: x - y,
+        'mult': lambda: x * y,
+        'div': lambda: x / y,
+    }.get(operator, lambda: None)()
+
+
+print(dispatch_dict('mult', 2, 8))
+print(dispatch_dict('unknown', 2, 8))
