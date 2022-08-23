ce3d84c1fbacbb21aff2fbc3e50276e1c1f34775
1337tester <miso.zima@1337tester.com>
2022-08-23 22:00:27 +0100

    more deletions

--- Scripts/csvcko.py
+++ Scripts/csvcko.py
@@ -1,9 +1,13 @@
 import csv
-path = "gateways_20161124.csv"
-file = open(path, newline = '')
-reader = csv.reader(file)
-header = next(reader)
-data = [row for row in reader]
+import os
 
-print(header)
-print(data[0])
+script_dir = os.path.abspath( os.path.dirname( __file__ ) )
+filename = "gateways_20161124.csv"
+filepath = os.path.join(script_dir, filename)
+
+with open(filepath, newline = '') as file:
+    reader = csv.reader(file)
+    header = next(reader)
+    data = [row for row in reader]
+    print(header)
+    print(data[0])
--- instabot.py/startup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-date > disk_space_report.txt
