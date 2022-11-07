95bd782ee0a34df0d755e02e1ad8e643d360325f
Michal <miso.zima@1337tester.com>
2022-11-07 15:59:34 +0000

    Scraped first result page

--- web_scraping/freelancers_sele.py
+++ web_scraping/freelancers_sele.py
@@ -1,11 +1,34 @@
 from selenium import webdriver
-from selenium.webdriver import Keys #ActionChains
-# from  selenium.webdriver.support.ui import Select
+# from selenium.webdriver import Keys #ActionChains
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.common.exceptions import NoSuchElementException
+from bs4 import BeautifulSoup
 from time import sleep
+
+separator = 40*"*"
+
+def jobs_info(job):
+    job_details = [text for text in job.stripped_strings]
+    
+    # deleting premium content
+    del job_details[1]
+    del job_details[1]
+    del job_details[-1]
+    del job_details[-1]
+
+    print(*job_details, sep = "\n")
+    print(separator)
+    
+def check_pagination(pagination):
+    pages = pagination.text.split(' ')
+    print(pages[1], pages[2], pages[3])
+    # print(pages)
+
+
 
 # cookie_banner_css = '#cookieChoiceInfo > div > span.cookie-choices-text'
 # cookie_dismiss_xpath = '//*[@id="cookieChoiceDismiss"]'
@@ -16,7 +39,9 @@ city_selectfirst_css = '.place-autocompleter > a:nth-child(1)'
 city_distance_css = 'div.col-sm-2:nth-child(3) > div:nth-child(2) > button:nth-child(1)'
 city_distance_100km_css = 'div.col-sm-2:nth-child(3) > div:nth-child(2) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(5) > a:nth-child(1)'
 search_css = '#search_simple'
-
+job_info_css = '.project-list > div'
+pagination_css = '[id=pagination] > p'
+next_css = '[aria-label=Next]'
 
 chrome_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
@@ -31,9 +56,9 @@ try:
     input_city.click()
     sleep(1)
     input_city.send_keys("Zürich")
-    sleep(1)
 
     # select first choice
+    WebDriverWait(chrome_driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, city_selectfirst_css)))
     city_dropdown = chrome_driver.find_element(By.CSS_SELECTOR, city_selectfirst_css)
     city_dropdown.click()
     
@@ -45,10 +70,19 @@ try:
     # submit search
     chrome_driver.find_element(By.CSS_SELECTOR, search_css).click()    
     
-    sleep(10)
+    WebDriverWait(chrome_driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, job_info_css)))
+    jobs = chrome_driver.find_elements(By.CSS_SELECTOR, job_info_css)
+    for job in jobs:
+        soup_job = BeautifulSoup(job.get_attribute('innerHTML'), 'html.parser')
+        jobs_info(soup_job)
+
+    pagination = chrome_driver.find_element(By.CSS_SELECTOR, pagination_css)
+    check_pagination(pagination)
+    
+    
     
 except NoSuchElementException:
-    print('aaa')
+    print('Element not found')
     
 finally:
     chrome_driver.close()
