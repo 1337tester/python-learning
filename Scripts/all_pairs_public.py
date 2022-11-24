60efb034b6b878ea314e4324c5d5d40799b70280
Michal <miso.zima@1337tester.com>
2022-11-24 23:03:25 +0000

    Prepare allpairs for presentation

--- Scripts/all_pairs_public.py
+++ Scripts/all_pairs_public.py
@@ -6,7 +6,7 @@ parameters = [
     ['16GB', '24GB', '64GB'],
     ['6core', '8core', '12core'],
     ['Slow', 'Medium', 'RTX3090'],
-    ['HDD', 'SDD', 'HDD+SDD']    
+    ['HDD', 'SDD', 'HDD+SDD']
 ]
 
 for i, pairs in enumerate(AllPairs(parameters)): print("{:2d}: {}".format(i, pairs))
